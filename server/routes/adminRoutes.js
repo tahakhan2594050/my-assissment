30562c628ffbc0cf7b43fdd837bc8aa10e7e3483
Muhammad Taha <tahakhan031121050@gmail.com>
2026-01-14 22:19:25 +0400

    Update adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,23 +1,23 @@
 import express from "express";
 import { adminAuth } from "../middlewares/adminAuth.js";
-import {
-    getAllUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-    getStats
-} from "../controllers/adminController.js";
+import { getAllUsers, createUser, updateUser, deleteUser, getAllCreations, deleteCreation, getSystemStats } from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
 
+// Admin authentication middleware for all routes
 adminRouter.use(adminAuth);
 
+// User management routes
 adminRouter.get('/users', getAllUsers);
-adminRouter.get('/users/:userId', getUserById);
 adminRouter.post('/users', createUser);
-adminRouter.put('/users/:userId', updateUser);
-adminRouter.delete('/users/:userId', deleteUser);
-adminRouter.get('/stats', getStats);
+adminRouter.put('/users/:id', updateUser);
+adminRouter.delete('/users/:id', deleteUser);
+
+// Content management routes
+adminRouter.get('/creations', getAllCreations);
+adminRouter.delete('/creations/:id', deleteCreation);
+
+// System statistics
+adminRouter.get('/stats', getSystemStats);
 
 export default adminRouter;
